66d7bed8dfed9363df12ab964fdb772684015990
nustxujun <nustxujun@gmail.com>
2024-06-28 00:39:11 +0800

    initial commit

---
+++ Source/Demo.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class DemoTarget : TargetRules
+{
+	public DemoTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("Demo");
+	}
+}
